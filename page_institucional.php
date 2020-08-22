d7beb14e464ab2bee585c23052927b4523fdc5a9
gnlaures <gn.laureano@gmail.com>
2020-08-22 16:59:58 -0300

    Institucional

--- page_institucional.php
+++ page_institucional.php
@@ -6,7 +6,7 @@
 				<p>A JRF Energy Brokers é uma empresa de Corretagem e Administração de Seguros, pertence ao grupo Energy Group e nasceu da fusão estratégica entre a Energy Broker, a JR Fernandes, a Ramires Fernandes Corretoras de Seguros e a Oxford-in Corretora de Seguros, que unem a sua experiência e elevada qualidade de serviços para atender ao segmento de seguros Premium, mantendo o alto padrão na gestão e corretagem de Seguros Corporativos. Esta união reforça a tradição, qualidade e soluções de mais de 70 anos de atuação no Mercado de Seguros Nacional e Internacional.</p>
 			</div>
 			<div class="s-company__picture">
-				img //
+				<img src="media/img/content/about.png" alt="JRF Energy Brokers">
 			</div>
 		</div>
 	</section>
