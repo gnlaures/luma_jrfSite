6ca69acc1b5b90d327ce62c777bb950d58d6d568
gnlaures <gn.laureano@gmail.com>
2020-08-23 22:29:29 -0300

    Matriz

--- page_matriz.php
+++ page_matriz.php
@@ -1,55 +1,77 @@
 <main id="page__matriz">
     <section class="s-matriz">
 		<div class="u-containerBig u-containerFlex">
-			<div class="s-matriz__secure">
-				<h1><strong>seguro</strong> automóvel</h1>
-				<p>A Corretora é especializada em seguros de Automóveis no segmento Premium e mantém importantes parcerias com as principais Concessionárias e Lojas de veículos de Luxo do País. Oferecemos os mais completos e inovadores seguros de Automóveis do Brasil, através de uma Equipe de Profissionais altamente qualificados, é realizada uma verdadeira consultoria na contratação e renovação de seus seguros.</p>
-				<p>Buscamos sempre as melhores coberturas e condições para satisfazer suas expectativas, em função da parceria junto as grandes seguradoras. Você conta com propostas estruturadas não só para indenizar prejuízos, mas para facilitar a sua vida e evitar que imprevistos aconteçam com você e seu carro.</p>
-				<p>Há um departamento de renovação especializado em seguros de automóveis que está a sua disposição full time, como também na ocorrência de eventual sinistro, para orientar e acompanhar, passo a passo, o processo até sua completa finalização; garantindo sua tranquilidade.</p>
+			<div class="s-matriz__l">
+				<div class="s-matriz__secure">
+					<h1><strong>seguro</strong> automóvel</h1>
+					<p>A Corretora é especializada em seguros de Automóveis no segmento Premium e mantém importantes parcerias com as principais Concessionárias e Lojas de veículos de Luxo do País. Oferecemos os mais completos e inovadores seguros de Automóveis do Brasil, através de uma Equipe de Profissionais altamente qualificados, é realizada uma verdadeira consultoria na contratação e renovação de seus seguros.</p>
+					<p>Buscamos sempre as melhores coberturas e condições para satisfazer suas expectativas, em função da parceria junto as grandes seguradoras. Você conta com propostas estruturadas não só para indenizar prejuízos, mas para facilitar a sua vida e evitar que imprevistos aconteçam com você e seu carro.</p>
+					<p>Há um departamento de renovação especializado em seguros de automóveis que está a sua disposição full time, como também na ocorrência de eventual sinistro, para orientar e acompanhar, passo a passo, o processo até sua completa finalização; garantindo sua tranquilidade.</p>
+				</div>
+				<div class="s-matriz__cover">
+					<img src="media/img/content/matriz.png" alt="">
+				</div>
+				<div class="s-matriz__brands">
+					<h2>Algumas marcas que trabalhamos</h2>
+					<ul>
+						<li><img src="media/img/content/brand_1.png" alt="Lexus"></li>
+						<li><img src="media/img/content/brand_2.png" alt="Land Rover"></li>
+						<li><img src="media/img/content/brand_3.png" alt="Lamborghini"></li>
+						<li><img src="media/img/content/brand_4.png" alt="Jeep"></li>
+						<li><img src="media/img/content/brand_5.png" alt="Jaguar"></li>
+						<li><img src="media/img/content/brand_6.png" alt="Dodge"></li>
+						<li><img src="media/img/content/brand_7.png" alt=""></li>
+						<li><img src="media/img/content/brand_8.png" alt="Chrysler"></li>
+						<li><img src="media/img/content/brand_9.png" alt="BMW"></li>
+						<li><img src="media/img/content/brand_10.png" alt="Mitsubishi"></li>
+						<li><img src="media/img/content/brand_11.png" alt="Toyota"></li>
+						<li><img src="media/img/content/brand_12.png" alt="Ferrari"></li>
+						<li><img src="media/img/content/brand_13.png" alt="Masserati"></li>
+					</ul>
+				</div>
 			</div>
-			<div class="s-matriz__cover">
-				img //
-			</div>
-			<div class="s-matriz__advantages">
-				<h2>Confira algumas vantagens:</h2>
-				<ul>
-					<li><p>Descontos por perfil do segurado, valorizando dados do principal motorista como mulheres, pessoas casadas, que guardam o veículo em garagem, entre outros, atribuindo à eles descontos significativos;</p></li>
-					<li><p>Coberturas adicionais como carro-extra e planos de assistência 24 horas completos, com direito a remoção hospitalar e transporte de familiares.</p></li>
-				</ul>
-				<h2>Coberturas básicas</h2>
-				<ul>
-					<li><p>Garante o pagamento da indenização ao segurado em caso de incêndio, roubo e/ou colisão;</p></li>
-					<li><p>Assistência 24 horas ao veículo (níveis diferentes de contratação);</p></li>
-					<li><p>Danos aos vidros;</p></li>
-					<li><p>Responsabilidade Civil Facultativa a terceiros, danos materiais (valores diferentes de contratação);</p></li>
-					<li><p>Responsabilidade Civil Facultativa a terceiros, danos corporais (valores diferentes de contratação).</p></li>
-				</ul>
-				<h2>Coberturas adicionais</h2>
-				<ul>
-					<li><p>Assistência 24 horas no Brasil inteiro;</p></li>
-					<li><p>Reembolso de despesas extraordinárias;</p></li>
-					<li><p>Acidentes pessoais a passageiros;</p></li>
-					<li><p>Carro extra;</p></li>
-					<li><p>Franquia Reduzida (50% menos do valor cobrado no mercado);</p></li>
-					<li><p>Franquia Majorada (50% mais do valor cobrado no mercado);</p></li>
-					<li><p>Acessórios e equipamentos.</p></li>
-				</ul>
-				<h2>Seguro Carta Verde*</h2>
-				<p>Seguro obrigatório para automóveis matriculados no país de origem em viagem internacional no âmbito do Mercosul.</p>
-				<ul>
-					<li><p>Cobertura Responsabilidade Civil por danos pessoais e materiais causados a terceiros não transportados pelo veículo segurado.</p></li>
-					<li><p>Indenização Reembolso ao segurado, até a importância segurada, das quantias que tiver de pagar por ser civilmente responsável por acidente que causar.</p></li>
-				</ul>
-				<p class="obs">* O Seguro Carta Verde não está disponível para motocicletas.</p>
+			<div class="s-matriz__r">
+				<div class="s-matriz__advantages">
+					<h2>Confira algumas vantagens:</h2>
+					<br>
+					<ul>
+						<li><p>Descontos por perfil do segurado, valorizando dados do principal motorista como mulheres, pessoas casadas, que guardam o veículo em garagem, entre outros, atribuindo à eles descontos significativos;</p></li>
+						<li><p>Coberturas adicionais como carro-extra e planos de assistência 24 horas completos, com direito a remoção hospitalar e transporte de familiares.</p></li>
+					</ul>
+					<br>
+					<h2>Coberturas básicas</h2>
+					<br>
+					<ul>
+						<li><p>Garante o pagamento da indenização ao segurado em caso de incêndio, roubo e/ou colisão;</p></li>
+						<li><p>Assistência 24 horas ao veículo (níveis diferentes de contratação);</p></li>
+						<li><p>Danos aos vidros;</p></li>
+						<li><p>Responsabilidade Civil Facultativa a terceiros, danos materiais (valores diferentes de contratação);</p></li>
+						<li><p>Responsabilidade Civil Facultativa a terceiros, danos corporais (valores diferentes de contratação).</p></li>
+					</ul>
+					<br>
+					<h2>Coberturas adicionais</h2>
+					<br>
+					<ul>
+						<li><p>Assistência 24 horas no Brasil inteiro;</p></li>
+						<li><p>Reembolso de despesas extraordinárias;</p></li>
+						<li><p>Acidentes pessoais a passageiros;</p></li>
+						<li><p>Carro extra;</p></li>
+						<li><p>Franquia Reduzida (50% menos do valor cobrado no mercado);</p></li>
+						<li><p>Franquia Majorada (50% mais do valor cobrado no mercado);</p></li>
+						<li><p>Acessórios e equipamentos.</p></li>
+					</ul>
+					<br>
+					<h2>Seguro Carta Verde*</h2>
+					<br>
+					<p>Seguro obrigatório para automóveis matriculados no país de origem em viagem internacional no âmbito do Mercosul.</p>
+					<ul>
+						<li><p>Cobertura Responsabilidade Civil por danos pessoais e materiais causados a terceiros não transportados pelo veículo segurado.</p></li>
+						<li><p>Indenização Reembolso ao segurado, até a importância segurada, das quantias que tiver de pagar por ser civilmente responsável por acidente que causar.</p></li>
+					</ul>
+					<br>
+					<a href="#" class="c-btn --white">Faça uma simulação</a>
+				</div>
 			</div>
 		</div>
 	</section>
-	<div class="s-brands">
-		<div class="u-containerBig">
-			<h2>Algumas marcas que trabalhamos</h2>
-			<ul>
-				<li></li>
-			</ul>
-		</div>
-	</div>
 </main>
